d04df878ebcd057984ff3287c63e6345e4e71707
Dinesh <dineshrdk1999@gmail.com>
2023-04-07 18:43:42 +0530

    second change

--- mathi.java
+++ mathi.java
@@ -1,7 +1,5 @@
 class mathi{
     public static void main(String args[]){
-        int a=10;
-        int b=20;
-        System.out.println(a+b);
+        System.out.println("this is your project")
     }
 }
